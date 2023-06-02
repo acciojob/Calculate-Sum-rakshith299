64d30a39048b4a47b57f90055982eabe591306d5
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-02 21:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 function getSum(){
 	var no1 = parseInt(document.getElementById("n1").value);
 	var no2 = parseInt(document.getElementById("n2").value);
-	document.getElementById("Sum").value = String(no1 + no2);
+	document.getElementById("Sum").innerHTML = String(no1 + no2);
 }
 
 
