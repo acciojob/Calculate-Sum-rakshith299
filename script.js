10a094486ad5c61bb9517f8140d7e281a40cfb84
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-02 21:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var no1 = parseInt(document.getElementById("n1").value);
-var no2 = parseInt(document.getElementById("n2").value);
+
 
 
 
 function getSum(){
-	document.getElementById("sum").value = String(no1 + no2);
+	var no1 = parseInt(document.getElementById("n1").value);
+	var no2 = parseInt(document.getElementById("n2").value);
+	document.getElementById("Sum").value = String(no1 + no2);
 }
 
 
